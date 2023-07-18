1eddbd5758c01829b0cdd24a87ff69f5534043e0
talyahho24-meet <139688490+talyahho24-meet@users.noreply.github.com>
2023-07-18 16:26:30 +0300

    kjkugkhgk

--- Flask-Routing-Lab/app.py
+++ Flask-Routing-Lab/app.py
@@ -6,6 +6,13 @@ app = Flask(  # Create a flask app
     template_folder='templates',  # Name of html file folder
     static_folder='static'  # Name of directory for static files
 )
+@app.route("/")
+def index():
+    return render_template("home.html")
+    
+@app.route('/hello')
+def hello():
+    return render_template('index page')
 
 # Your code should be below
 
